95b555e63c86b0d0d42871cabd678c60bde3ba45
AbhishekPatil1310 <ap7497493@gmail.com>
2025-11-27 20:41:54 +0530

    Add videoUrl field to Chat model
    Added videoUrl field to message schema.

--- backend/src/models/Chat.model.js
+++ backend/src/models/Chat.model.js
@@ -5,8 +5,10 @@ const messageSchema = new mongoose.Schema({
 from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 text: String,
+  videoUrl: { type: String },
 createdAt: { type: Date, default: Date.now }
 });
+
 
 
 export default mongoose.model('Message', messageSchema);
