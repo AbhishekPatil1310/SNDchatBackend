38cc7a6735446be901489bfc7e53e5cb9fc1c287
AbhishekPatil1310 <ap7497493@gmail.com>
2025-11-27 20:41:36 +0530

    Update Chat.controller.js

--- backend/src/controller/Chat.controller.js
+++ backend/src/controller/Chat.controller.js
@@ -1,3 +1,4 @@
+import {uploadVideoToSupabase } from '../services/supabase.js'
 import Message from '../models/Chat.model.js';
 
 // GET conversation with a specific user
@@ -32,6 +33,35 @@ export const postMessage = async (req, res) => {
     res.status(201).json(message);
   } catch (err) {
     console.error('POST message error:', err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+export const uploadVideoMessage = async (req, res) => {
+  try {
+    const from = req.user.id;
+    const to = req.params.userId;
+
+    if (!req.file)
+      return res.status(400).json({ message: "No video file provided" });
+
+    const videoUrl = await uploadVideoToSupabase(
+      req.file.buffer,
+      `${Date.now()}-${req.file.originalname}`,
+      req.file.mimetype
+    );
+
+    const message = await Message.create({
+      from,
+      to,
+      videoUrl,
+    });
+
+    res.status(201).json(message);
+
+  } catch (err) {
+    console.error("Video upload error:", err);
     res.status(500).json({ message: err.message });
   }
 };
